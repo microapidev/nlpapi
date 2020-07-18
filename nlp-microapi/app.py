cda48e7a1de0c80cd78aff74164edc94a5b5036a
Henry <16284830+code2exe@users.noreply.github.com>
2020-07-18 19:32:37 +0100

    Changes

--- nlp-microapi/app.py
+++ nlp-microapi/app.py
@@ -28,5 +28,7 @@ def predict():
 
     return render_template('index.html', final=compound, text1=text1, processed=processed, lang=parse_lang)
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
+    app.run()
+    
 #     app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
