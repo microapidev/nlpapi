2b6d84ce466173571927647a4855d3d0d7a7d30e
Henry <16284830+code2exe@users.noreply.github.com>
2020-07-19 04:25:07 +0100

    Heroku Deployment

--- app.py
+++ app.py
@@ -32,3 +32,5 @@ def predict():
 #     app.run(host='127.0.0.1', port=8081, debug=True)
     
 #     app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
+if __name__ == "__main__": 
+    app.run()
